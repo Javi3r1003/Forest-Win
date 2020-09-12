d601eb023b1f75277d54ab83614cdcdadcbd3ff3
albertortega17 <69809447+albertortega17@users.noreply.github.com>
2020-09-11 23:17:21 -0600

    Entrega 11/09/2020
    Se comentó la clase Arbol y se cambió el nombre de las variables para evitar conflictos con la clase filtro.

--- Arbol.java
+++ Arbol.java
@@ -1,24 +1,40 @@
-import java.io.Serializable;
+/***************************************************************************
+									ForestWin
+Proyecto Programación Orientada a Objetos
+ Arbol.java (Clase Arbol)
+ Autores: Erick Raúl Alvarez Melgar - Carné 20900
+		  Adam Sebastian Rios Kirste - Carné 20616
+		  Juan Pablo Zelada Ramirez - Carné 201004
+		  César Rodrigro Meza Torres - Carné 20287
+		  Alberto Antonio Ortega Romero - Carné 20884
+		  Javier Alejandro Mejía Alecio - Carné 20304
 
+Con esta clase se implemento la clase Serializable para poder guardar los 
+objetos creados en un documento y poder crear así una base de datos
+ ****************************************************************************/
+
+//importamos la clase serializable
+import java.io.Serializable;
+//Clase Arbol
 public class Arbol implements Serializable {
-    //Atributos
-    String Especie;
-    float PrMax;
-    float PrMin;
-    String Iluminacion;
-    float TempMax;
-    float TempMin;
-    String Utilidades;
+  //Atributos
+    private String Especie;
+    private float Precipitacion;
+    private float DeltaPre;
+    private String Iluminacion;
+    private float Temperatura;
+    private float DeltaTem;
+    private String Utilidades;
     //**********************************************************************
 
     //Constructor Arbol
-    public Arbol(String especie, float prMax, float prMin, String iluminacion, float tempMax, float tempMin, String utilidades){
+    public Arbol(String especie, float precipitacion, float deltaPre, String iluminacion, float temperatura, float deltaTem, String utilidades){
         this.Especie = especie;
-        this.PrMax = prMax;
-        this.PrMin = prMin;
+        this.Precipitacion = precipitacion;
+        this.DeltaPre = deltaPre;
         this.Iluminacion = iluminacion;
-        this.TempMax = tempMax;
-        this.TempMin = tempMin;
+        this.Temperatura = temperatura;
+        this.DeltaTem = deltaTem;
         this.Utilidades = utilidades;
     }
 
@@ -27,20 +43,20 @@ public class Arbol implements Serializable {
     public String getEspecie() {
         return Especie;
     }
-    public float getPrMax() {
-        return PrMax;
+    public float getPrecipitacion() {
+        return Precipitacion;
     }
-    public float getPrMin() {
-        return PrMin;
+    public float getDeltaPre() {
+        return DeltaPre;
     }
     public String getIluminacion() {
         return Iluminacion;
     }
-    public float getTempMax() {
-        return TempMax;
+    public float getTemperatura() {
+        return Temperatura;
     }
-    public float getTempMin() {
-        return TempMin;
+    public float getDeltaTem() {
+        return DeltaTem;
     }
     public String getUtilidades() {
         return Utilidades;
@@ -51,20 +67,20 @@ public class Arbol implements Serializable {
     public void setEspecie(String especie) {
         Especie = especie;
     }
-    public void setPrMax(float prMax) {
-        PrMax = prMax;
+    public void setPrecipitacion(float precipitacion) {
+        Precipitacion = precipitacion;
     }
-    public void setPrMin(float prMin) {
-        PrMin = prMin;
+    public void setDeltaPre(float deltaPre) {
+        DeltaPre = deltaPre;
     }
     public void setIluminacion(String iluminacion) {
         Iluminacion = iluminacion;
     }
-    public void setTempMax(float tempMax) {
-        TempMax = tempMax;
+    public void setTemperatura(float temperatura) {
+        Temperatura = temperatura;
     }
-    public void setTempMin(float tempMin) {
-        TempMin = tempMin;
+    public void setDeltaTem(float deltaTem) {
+        DeltaTem = deltaTem;
     }
     public void setUtilidades(String utilidades) {
         Utilidades = utilidades;
