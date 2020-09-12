ca6010af8c3e91208176f40bf963e0a04d75b8eb
AdamKIKI <71142916+AdamKIKI@users.noreply.github.com>
2020-09-11 23:37:49 -0600

    Entrega 11/09/2020
    Se implementó una clase serializar para poder pasar los objetos a un archivo .txt y se comentó el código.

--- usuario.java
+++ usuario.java
@@ -1,15 +1,34 @@
+/***************************************************************************
+									ForestWin
+Proyecto Programación Orientada a Objetos
+ usuario.java (Clase usuario)
+ Autores: Erick Raúl Alvarez Melgar - Carné 20900
+		  Adam Sebastian Rios Kirste - Carné 20616
+		  Juan Pablo Zelada Ramirez - Carné 201004
+		  César Rodrigro Meza Torres - Carné 20287
+		  Alberto Antonio Ortega Romero - Carné 20884
+		  Javier Alejandro Mejía Alecio - Carné 20304
+
+Con esta clase se implemento la clase Serializable para poder guardar los 
+objetos creados en un documento y poder crear así una base de datos
+ ****************************************************************************/
+ 
+ //Se importa la clase Serializable 
 import java.io.Serializable;
 
-
+//Clase Usuario
 class usuario implements Serializable {
-	String nombre;
-	String contrasena;
+	//Atributos
+	private String nombre;
+	private String contrasena;
 	
+	//Constructor de la clase
 	usuario(String n, String c){
 		nombre = n;
 		contrasena = c;
 	}
 	
+	//getters de la clase
 	public String getNombre(){
 		return nombre;
 	}
