9143e1cd5bec95652ca324666db2794300966e7f
Rodrigomeza542 <69809969+Rodrigomeza542@users.noreply.github.com>
2020-09-11 23:13:26 -0600

    Entrega 11/09/2020
    Se le agregaron los métodos de la clase filtro y se comento el código

--- main.java
+++ main.java
@@ -1,29 +1,50 @@
+/***************************************************************************
+									ForestWin
+Proyecto Programación Orientada a Objetos
+ main.java (Clase main)
+ Autores: Erick Raúl Alvarez Melgar - Carné 20900
+		  Adam Sebastian Rios Kirste - Carné 20616
+		  Juan Pablo Zelada Ramirez - Carné 201004
+		  César Rodrigro Meza Torres - Carné 20287
+		  Alberto Antonio Ortega Romero - Carné 20884
+		  Javier Alejandro Mejía Alecio - Carné 20304
 
+ Con esta clase podremos ingresar a os métodos de impresión de la clase.
+ vista para poder mostrarle el menú al usuario que esté interactuando con el programa
+ ****************************************************************************/
 class main{
+	//metodo main
 	public static void main(String[] args){
+		//instancia de la clase vista
 		vista v = new vista();
 		
+		
+		//opciones del menú principal
 		int opcion = 0;
 		while(opcion != 5){
 			opcion = v.menu();
 			
 			if(opcion == 1){
 				if(v.login()){
+					
+					
+					//opciones del sub menú
 					int subOpcion = 0;
-					while(subOpcion != 5){
+					while(subOpcion != 6){
 					
 						subOpcion = v.subMenu();
 						
 						if(subOpcion == 1){
 							v.mostrarArboles();
 						}else if(subOpcion == 2){
-							v.leerArboles();
+							//filtro
 						}else if(subOpcion == 3){
-							//aqui iria lo de eliminar
+							v.agregarArbol();
 						}else if(subOpcion == 4){
-							if(v.salirPerfil()){
-								subOpcion = 5;
-							}
+							v.eliminarArbol();
+							
+						}else if(v.salirPerfil()){
+								subOpcion = 6;
 								
 						}
 					}
