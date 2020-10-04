b28fea49788cacd31871c0cde384a53da2feba63
JPZelada <71154920+JPZelada@users.noreply.github.com>
2020-10-04 16:01:42 -0600

    Modificaciones
    Se hace que el método main dispare el programa

--- main.java
+++ main.java
@@ -16,51 +16,12 @@ class main{
 	//metodo main
 	public static void main(String[] args){
 		//instancia de la clase vista
-		vista v = new vista();
+		controlador control = new controlador();
 		
 		
-		//opciones del menú principal
-		int opcion = 0;
-		while(opcion != 5){
-			opcion = v.menu();
-			
-			if(opcion == 1){
-				if(v.login()){
-					
-					
-					//opciones del sub menú
-					int subOpcion = 0;
-					while(subOpcion != 6){
-					
-						subOpcion = v.subMenu();
-						
-						if(subOpcion == 1){
-							v.mostrarArboles();
-						}else if(subOpcion == 2){
-							v.filtrosDeBusqueda();
-						}else if(subOpcion == 3){
-							v.agregarArbol();
-						}else if(subOpcion == 4){
-							v.eliminarArbol();
-							
-						}else if(v.salirPerfil() == true){
-								subOpcion = 6;
-								
-						}
-					}
-				}
-			}else if(opcion == 2){
-				v.crear();
-			}else if(opcion ==3){
-				v.mostrarUsuarios();
-				
-			}else if(opcion == 4){
-				if(v.salirPrograma() == true){
-					opcion = 5;
-				}
-			}
-		
-		}
+		//llamar al método para correr todo el programa
+		control.programa();
+
 		
 		
 	}
