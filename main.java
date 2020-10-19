78d87eab0b632357af7841866c434bde89e57508
JPZelada <71154920+JPZelada@users.noreply.github.com>
2020-10-18 22:17:41 -0600

    Encabezado casi final
    Ya terminando el proyecto se describen los encabezados casi finales de las clases.

--- main.java
+++ main.java
@@ -1,16 +1,16 @@
 /***************************************************************************
-									ForestWin
+ForestWin
 Proyecto Programación Orientada a Objetos
- main.java (Clase main)
- Autores: Erick Raúl Alvarez Melgar - Carné 20900
+*main.java (Clase main)
+*Autores: Erick Raúl Alvarez Melgar - Carné 20900
 		  Adam Sebastian Rios Kirste - Carné 20616
 		  Juan Pablo Zelada Ramirez - Carné 201004
 		  César Rodrigro Meza Torres - Carné 20287
 		  Alberto Antonio Ortega Romero - Carné 20884
 		  Javier Alejandro Mejía Alecio - Carné 20304
 
- Con esta clase podremos ingresar a os métodos de impresión de la clase.
- vista para poder mostrarle el menú al usuario que esté interactuando con el programa
+ En esa clase unicamente se llamará a la clase controlador y así poder
+ iniciar todo el programa.
  ****************************************************************************/
 class main{
 	//metodo main
