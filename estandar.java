dd2d6289080e43a72fdb21c6445f41c48667fb4d
Javier Alecio <63657473+Javi3r1003@users.noreply.github.com>
2020-10-18 22:23:36 -0600

    Encabezado
    Se añadio el encabezado

--- estandar.java
+++ estandar.java
@@ -1,4 +1,16 @@
+/***************************************************************************
+ForestWin
+Proyecto Programación Orientada a Objetos
+*estandar.java (Clase estandar)
+*Autores: Erick Raúl Alvarez Melgar - Carné 20900
+		  Adam Sebastian Rios Kirste - Carné 20616
+		  Juan Pablo Zelada Ramirez - Carné 201004
+		  César Rodrigro Meza Torres - Carné 20287
+		  Alberto Antonio Ortega Romero - Carné 20884
+		  Javier Alejandro Mejía Alecio - Carné 20304
 
+Esta clase es derivada de la clase usuario, utiliza sus mismos métodos y atributos
+ ****************************************************************************/
 
 class estandar extends usuario{
 	
