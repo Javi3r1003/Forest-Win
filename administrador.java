6f3bed82987c4bcc31775fa7d4f5bb4c45a64323
AdamKIKI <71142916+AdamKIKI@users.noreply.github.com>
2020-10-18 23:01:14 -0600

    Se hizo el encabezado de la clase
    Se agregó el encabezado de la clase

--- administrador.java
+++ administrador.java
@@ -1,3 +1,17 @@
+/***************************************************************************
+ForestWin
+Proyecto Programación Orientada a Objetos
+*administrador.java (Clase administrador)
+*Autores: Erick Raúl Alvarez Melgar - Carné 20900
+		  Adam Sebastian Rios Kirste - Carné 20616
+		  Juan Pablo Zelada Ramirez - Carné 201004
+		  César Rodrigro Meza Torres - Carné 20287
+		  Alberto Antonio Ortega Romero - Carné 20884
+		  Javier Alejandro Mejía Alecio - Carné 20304
+
+Esta clase es derivada de la clase usuario, utiliza sus mismos métodos y atributos
+ ****************************************************************************/
+ 
 
 class administrador extends estandar{
 	public administrador(String nom, String con){
